cd7f0d4da7c4d8ddef08ad866f9b767569429615
Andy Doan <andy@opensourcefoundries.com>
2018-06-06 16:07:42 -0500

    Add bare-bones login logic
    This doesn't work, but gets things in place for something that does
    
    requires pip3 install flask_login
    
    This is being added to a gavel_db module so that it will eventually
    be able to be shared with another flask app as common code.
    
    Signed-off-by: Andy Doan <andy@opensourcefoundries.com>

--- gavel_ci/flask.py
+++ gavel_ci/flask.py
@@ -12,9 +12,18 @@ def create_app(settings_object='gavel_ci.settings'):
     app.wsgi_app = ProxyFix(app.wsgi_app)
     app.config.from_object(settings_object)
 
-    from gavel_ci.models import db
+    from gavel_ci.models import db, User
     db.init_app(app)
     Migrate(app, db)
+
+    from flask_login import LoginManager
+    login_manager = LoginManager(app)
+    login_manager.login_view = 'app.login'
+    login_manager.session_protection = 'strong'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     import gavel_ci.ui
     gavel_ci.ui.register_blueprints(app)
