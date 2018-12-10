a815053a48292beaa48c7f0582c6835f3b0969bf
Andy Doan <andy@foundries.io>
2018-12-09 23:54:42 -0600

    Add a way to authenticate REST API with tokens
    The JobServ API doesn't know about the Gavel CI DB so we have to
    insert some nginx proxy logic to intercept an API Token and convert
    it into a real token if necessary.
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- gavel_ci/ui/settings.py
+++ gavel_ci/ui/settings.py
@@ -4,6 +4,8 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, fresh_login_required, login_required
 
+from gavel_ci.models import ApiToken
+
 blueprint = Blueprint('settings', __name__, url_prefix='/settings')
 
 
@@ -34,3 +36,26 @@ def token_create():
     flash(
         'Make sure you save the value, you won\'t be able to access it again.')
     return redirect(url_for('settings.index'))
+
+
+@blueprint.route('/token/auth/')
+def token_auth():
+    # Convert a valid API token into a JWT bearer
+    t = request.headers.get('Authorization')
+    if t:
+        parts = t.split(' ', 2)
+        if len(parts) == 2 and parts[0] == 'token':
+            parts = parts[1].split(':')
+            if len(parts) == 2:
+                user, token = parts
+                u = ApiToken.find_user(user, token)
+                if u:
+                    return (
+                        'OK',
+                        {'Authorization': 'Bearer ' + u.authorization_bearer()}
+                    )
+                return 'No such user for this token', 401
+        # this could be a runner or worker accessing the API and JobServ
+        # can handle that
+        return '0k', {'Authorization': t}
+    return 'ok'
