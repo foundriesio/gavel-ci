f553d33b008625a7c578965bb1096a0b033063fc
Andy Doan <andy@foundries.io>
2018-12-09 21:52:47 -0600

    Add a settings page for a user
    Signed-off-by: Andy Doan <andy@foundries.io>

--- gavel_ci/ui/__init__.py
+++ gavel_ci/ui/__init__.py
@@ -3,10 +3,12 @@
 
 from gavel_ci.ui.auth import blueprint as auth_bp
 from gavel_ci.ui.jobserv import blueprint as jobserv_bp
+from gavel_ci.ui.settings import blueprint as settings_bp
 
 BLUEPRINTS = (
     auth_bp,
     jobserv_bp,
+    settings_bp,
 )
 
 
